35cb3071ddf0ef479bce5a4172cb60b7a24a96d6
Michael Lin <51706220+michlin0825@users.noreply.github.com>
2019-10-01 11:45:12 +0800

    Delete create.sh

--- cfn/create.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-aws cloudformation create-stack \
---stack-name $1 \
---template-body file://$2 \
---parameters file://$3 \
---region us-east-1 \
---capabilities CAPABILITY_NAMED_IAM
