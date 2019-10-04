0f1a8a14db6afce0d01e397a1de52eb26b000f68
Michael Lin <51706220+michlin0825@users.noreply.github.com>
2019-10-05 01:11:54 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ APP = Flask(__name__)
 @APP.route("/")
 def home():
     """does more things"""
-    html = f"<html><h3>Hello Udacity!</h3><p>This is Michael Lin.<br><font color='green'>I love green.</font></p></html>"
+    html = f"<html><p>This is Michael Lin.<br><font color='green'>I love green.</font></p></html>"
     return html.format(format)
 
 
 if __name__ == "__main__":
-    APP.run(host='0.0.0.0', port=80, debug=True) 
+    APP.run(host='0.0.0.0', port=80, debug=True)
